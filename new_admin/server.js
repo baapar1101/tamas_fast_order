a3b83b0f82ec35000b13942ceac2007b5fda82c2
Albert Heidarian <50908145+baapar1101@users.noreply.github.com>
2026-09-21 09:37:28 +0330

    no

--- new_admin/server.js
+++ new_admin/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
